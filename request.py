264e9771829ca4609666868b4fac4e3d13454ca5
Joshua Paracuelles <joshuaparacuelles5@gmail.com>
2026-09-22 18:29:58 +0800

    Aaa

--- request.py
+++ request.py
@@ -10,7 +10,7 @@ from flask_cors import CORS
 from supabase_client import supabase
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["https://client-ov0j76xwg-joshua-paracuelles-projects.vercel.app"])
 
 BUCKET = "signatures"
 MAX_SIG_BYTES = 2 * 1024 * 1024
